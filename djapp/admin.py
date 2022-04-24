dcd26d56f10d76a1c4007370f3fb20179416507c
reem-elbakry <reemmousa1995@gmail.com>
2022-04-25 01:48:12 +0200

    register models to admin

--- djapp/admin.py
+++ djapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student, Track
 
-# Register your models here.
+# Register your models here.  (in admin site)
+
+admin.site.register(Student)
+admin.site.register(Track)
+
+
