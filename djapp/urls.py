f108f358667a98ce1bb13719bc328c2815929921
reem-elbakry <reemmousa1995@gmail.com>
2022-04-26 01:35:54 +0200

    using name with url

--- djapp/urls.py
+++ djapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('show/<std_id>', views.show),
+    path('home/', views.home, name='home'),
+    path('details/<std_id>', views.show, name='show'),
 
 ]
